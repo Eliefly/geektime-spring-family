a1fea9b8916a2758afdc5a55ee6dde8e3944b162
eliefly <eliefly@163.com>
2019-07-15 00:47:22 +0800

    update

--- Chapter 7/complex-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
+++ Chapter 7/complex-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
@@ -15,61 +15,63 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import javax.print.attribute.standard.Media;
-import java.math.BigDecimal;
-import java.net.URI;
 import java.util.List;
 
 @SpringBootApplication
 @Slf4j
 public class CustomerServiceApplication implements ApplicationRunner {
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder()
-				.sources(CustomerServiceApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.NONE)
-				.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(CustomerServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE)
+                .run(args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
 //		return new RestTemplate();
-		return builder.build();
-	}
+        return builder.build();
+    }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		URI uri = UriComponentsBuilder
-				.fromUriString("http://localhost:8080/coffee/?name={name}")
-				.build("mocha");
-		RequestEntity<Void> req = RequestEntity.get(uri)
-				.accept(MediaType.APPLICATION_XML)
-				.build();
-		ResponseEntity<String> resp = restTemplate.exchange(req, String.class);
-		log.info("Response Status: {}, Response Headers: {}", resp.getStatusCode(), resp.getHeaders().toString());
-		log.info("Coffee: {}", resp.getBody());
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("http://localhost:8080/coffee/?name={name}")
+//                .build("mocha");
+//        // 构造请求体并配置header
+//        RequestEntity<Void> req = RequestEntity.get(uri)
+//                .accept(MediaType.APPLICATION_XML)
+//                .build();
+//        ResponseEntity<String> resp = restTemplate.exchange(req, String.class);
+//        log.info("Response Status: {}, Response Headers: {}", resp.getStatusCode(), resp.getHeaders().toString());
+//        log.info("Coffee: {}", resp.getBody());
 
-		String coffeeUri = "http://localhost:8080/coffee/";
-		Coffee request = Coffee.builder()
-				.name("Americano")
-				.price(Money.of(CurrencyUnit.of("CNY"), 25.00))
-				.build();
-		Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
-		log.info("New Coffee: {}", response);
+        String coffeeUri = "http://localhost:8080/coffee/";
+        Coffee request = Coffee.builder()
+                .name("Americano")
+                .price(Money.of(CurrencyUnit.of("CNY"), 25.00))
+                .build();
+        Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
+        log.info("New Coffee: {}", response);
 
-		ParameterizedTypeReference<List<Coffee>> ptr =
-				new ParameterizedTypeReference<List<Coffee>>() {};
-		ResponseEntity<List<Coffee>> list = restTemplate
-				.exchange(coffeeUri, HttpMethod.GET, null, ptr);
-		list.getBody().forEach(c -> log.info("Coffee: {}", c));
-	}
+        ParameterizedTypeReference<List<Coffee>> ptr = new ParameterizedTypeReference<List<Coffee>>() {
+        };
+        ResponseEntity<List<Coffee>> list = restTemplate.exchange(coffeeUri, HttpMethod.GET, null, ptr);
+        list.getBody().forEach(c -> log.info("Coffee: {}", c));
+
+        // 假设不使用 ParameterizedTypeReference<T> 解析泛型
+//        List<Coffee> coffees = new ArrayList<>();
+//        ResponseEntity<? extends List> responseEntity = restTemplate.exchange(coffeeUri, HttpMethod.GET, null, coffees.getClass());
+//        responseEntity.getBody().forEach(c -> log.info("Coffee: {}", c));
+
+//        coffees = restTemplate.getForObject(coffeeUri, coffees.getClass());
+//        coffees.forEach(c -> log.info("Coffee: {}", c));
+    }
 }
--- Chapter 7/simple-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
+++ Chapter 7/simple-resttemplate-demo/src/main/java/geektime/spring/springbucks/customer/CustomerServiceApplication.java
@@ -21,41 +21,41 @@ import java.net.URI;
 @SpringBootApplication
 @Slf4j
 public class CustomerServiceApplication implements ApplicationRunner {
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder()
-				.sources(CustomerServiceApplication.class)
-				.bannerMode(Banner.Mode.OFF)
-				.web(WebApplicationType.NONE)
-				.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(CustomerServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE) // 不会启动tomcat
+                .run(args);
+    }
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-//		return new RestTemplate();
-		return builder.build();
-	}
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return new RestTemplate();
+//        return builder.build();
+    }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		URI uri = UriComponentsBuilder
-				.fromUriString("http://localhost:8080/coffee/{id}")
-				.build(1);
-		ResponseEntity<Coffee> c = restTemplate.getForEntity(uri, Coffee.class);
-		log.info("Response Status: {}, Response Headers: {}", c.getStatusCode(), c.getHeaders().toString());
-		log.info("Coffee: {}", c.getBody());
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:8080/coffee/{id}")
+                .build(1);
+        ResponseEntity<Coffee> c = restTemplate.getForEntity(uri, Coffee.class);
+        log.info("Response Status: {}, Response Headers: {}", c.getStatusCode(), c.getHeaders().toString());
+        log.info("Coffee: {}", c.getBody());
 
-		String coffeeUri = "http://localhost:8080/coffee/";
-		Coffee request = Coffee.builder()
-				.name("Americano")
-				.price(BigDecimal.valueOf(25.00))
-				.build();
-		Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
-		log.info("New Coffee: {}", response);
+        String coffeeUri = "http://localhost:8080/coffee/";
+        Coffee request = Coffee.builder()
+                .name("Americano")
+                .price(BigDecimal.valueOf(25.00))
+                .build();
+        Coffee response = restTemplate.postForObject(coffeeUri, request, Coffee.class);
+        log.info("New Coffee: {}", response);
 
-		String s = restTemplate.getForObject(coffeeUri, String.class);
-		log.info("String: {}", s);
-	}
+        String s = restTemplate.getForObject(coffeeUri, String.class);
+        log.info("String: {}", s);
+    }
 }
